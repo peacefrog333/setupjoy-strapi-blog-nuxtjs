8058e40243506ec522372f7b0de874df773f20ab
Ali Azwad <ali.azwad@nspa.gov.mv>
2020-07-17 20:34:24 +0800

    add simple footer and change link color

--- components/elements/BlogPost.vue
+++ components/elements/BlogPost.vue
@@ -8,7 +8,7 @@
 
       <div class="max-w-screen-md px-4 mx-auto mt-5 text-lg leading-relaxed text-gray-700 lg:px-0">
           <nuxt-link :to="{path: '/', query: {category: category.title}}" 
-            class="inline-flex items-center justify-center px-4 py-1 mb-2 mr-2 text-gray-200 bg-black" 
+            class="px-4 py-1 bg-transparent text-teal-600 inline-flex items-center border border-teal-600 rounded-full justify-center mb-2 mr-2 text-base" 
             v-for='category in blog.blog_categories' :key='category.id'>{{category.title}}</nuxt-link>
             <div class='text-sm'>Blog posted on {{blogPostDate}}</div>
           <h2 class="mt-2 mb-4 text-4xl font-semibold leading-tight text-gray-800">
--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,12 @@
         </nuxt-link>
       </div>
     </div>
-    <Nuxt />
+    <Nuxt class='min-h-full ' />
+    <div class="container mx-auto ">
+      <div class="my-10 text-center">
+        <div class='text-sm mt-2 text-gray-600'> Made with 🧡 from Maldives</div>
+      </div>
+    </div>
   </div>
 </template>
 
